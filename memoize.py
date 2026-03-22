cecd0a3eaf858ce1541941eebdf080e507fc73e0
Kyrylo Kuznetsov <wyxxnt@gmail.com>
2026-03-22 19:07:35 +0100

    Started creating memoize

---
+++ memoize.py
@@ -0,0 +1,11 @@
+import time
+def wrapper(*args):
+        key = args
+
+        if key in cache:
+            if ttl is not None:
+                if time.time() - access_times[key] > ttl:
+                    del cache[key]
+                    del access_times[key]
+                    del access_counts[key]
+                    insert_order.remove(key)
