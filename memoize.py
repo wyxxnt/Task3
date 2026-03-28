1e8b8bb3574d10d8d597bfd65da6edc27a889fbe
Kyrylo Kuznetsov <wyxxnt@gmail.com>
2026-03-28 21:35:32 +0100

    Update memoize.py

--- memoize.py
+++ memoize.py
@@ -18,3 +18,26 @@ def memoize(func, max_size=None, policy="lru", ttl=None):
                 times[key] = time.time()
                 counts[key] += 1
                 return cache[key]
+                key]
+
+        if max_size and len(cache) >= max_size:
+            if policy == "lru":
+                old = min(cache, key=lambda k: times[k])
+            elif policy == "lfu":
+                old = min(cache, key=lambda k: counts[k])
+            else:
+                old = list(cache.keys())[0]
+            del cache[old]
+            del times[old]
+            del counts[old]
+
+        result = func(*args)
+        cache[key] = result
+        times[key] = time.time()
+        counts[key] = 1
+        return result
+
+    wrapper.cache = cache
+    wrapper.clear = lambda: (cache.clear(), times.clear(), counts.clear())
+
+    return wrapper
